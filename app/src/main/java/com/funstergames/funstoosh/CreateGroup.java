846fc043cf73b55e0f586e15fe8ca243e17b35b9
Hila <hila.enoch@gmail.com>
2017-01-16 07:21:14 +0200

    Rejected commit: Default

---
+++ app/src/main/java/com/funstergames/funstoosh/CreateGroup.java
@@ -0,0 +1,80 @@
+package com.funstergames.funstoosh;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class CreateGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+
+
+
+        //This class provides applications access to the content model.
+        ContentResolver cr = getApplicationContext().getContentResolver();
+
+//RowContacts for filter Account Types
+        Cursor contactCursor = cr.query(
+                ContactsContract.RawContacts.CONTENT_URI,
+                new String[]{ContactsContract.RawContacts._ID,
+                        ContactsContract.RawContacts.CONTACT_ID},
+                ContactsContract.RawContacts.ACCOUNT_TYPE + "= ?",
+                new String[]{"com.whatsapp"},
+                null);
+
+//ArrayList for Store Whatsapp Contact
+        ArrayList<String> myWhatsappContacts = new ArrayList<>();
+
+        if (contactCursor != null) {
+            if (contactCursor.getCount() > 0) {
+                if (contactCursor.moveToFirst()) {
+                    do {
+                        //whatsappContactId for get Number,Name,Id ect... from  ContactsContract.CommonDataKinds.Phone
+                        String whatsappContactId = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.RawContacts.CONTACT_ID));
+
+                        if (whatsappContactId != null) {
+                            //Get Data from ContactsContract.CommonDataKinds.Phone of Specific CONTACT_ID
+                            Cursor whatsAppContactCursor = cr.query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                    new String[]{ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                                            ContactsContract.CommonDataKinds.Phone.NUMBER,
+                                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                    new String[]{whatsappContactId}, null);
+
+                            if (whatsAppContactCursor != null) {
+                                whatsAppContactCursor.moveToFirst();
+                                String id = whatsAppContactCursor.getString(whatsAppContactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+                                String name = whatsAppContactCursor.getString(whatsAppContactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                                String number = whatsAppContactCursor.getString(whatsAppContactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                                whatsAppContactCursor.close();
+
+                                //Add Number to ArrayList
+                                myWhatsappContacts.add(number);
+
+                              //  showLogI(TAG, " WhatsApp contact id  :  " + id);
+                              //  showLogI(TAG, " WhatsApp contact name :  " + name);
+                              //  showLogI(TAG, " WhatsApp contact number :  " + number);
+                            }
+                        }
+                    } while (contactCursor.moveToNext());
+                    contactCursor.close();
+                }
+            }
+        }
+
+      //  showLogI(TAG, " WhatsApp contact size :  " + myWhatsappContacts.size());
+
+
+        
+
+    }
+}
